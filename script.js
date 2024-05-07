5b5d505bb285e1ecf3ae9109611b3d24fa16f510
Daniel Rodrigues <96748212+danniel-rodrigues@users.noreply.github.com>
2024-05-06 22:44:32 -0300

    Add animações aos elementos da página

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// Adapta o Header quando o usuário faz scroll na página
 window.addEventListener("scroll", function() {
     const header = document.getElementById('header')
     header.classList.toggle('scroll', window.scrollY > 0)
 })
+
+
+const intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+    })
+})
+
+const elements = document.querySelectorAll('.animate')
+elements.forEach(el => intersectionObserver.observe(el))
